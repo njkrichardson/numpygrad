c60b87a25fe7beac4db8edc5f9b065e4f32f7f15
Nick Richardson <njkrichardson@princeton.edu>
2026-02-25 22:34:47 -0500

    [maintenance]: refactors mlp.

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from numpy import require
 import numpygrad as npg
-from numpygrad.nn import Linear
+from numpygrad.nn import Linear, MLP
 
 npg.manual_seed(0)
 Log = npg.Log(__name__)
 
 
 def main():
+    hidden_sizes = [4, 3]
+    input_dim = 3
+    output_dim = 1
+    net = MLP(input_dim, hidden_sizes, output_dim)
+
     batch_size: int = 2
-    # net = Linear(3, 2)
-    x = npg.randn((batch_size, 3), requires_grad=True)
-    y = x.reshape(-1)
-    out = y.sum()
+    x = npg.randn((batch_size, input_dim), requires_grad=True)
+    out = net(x).sum()
+    print(out)
     out.backward()
-    print(x.shape)
-    print(y.shape)
-    print(x.grad)
+    for param in net.parameters():
+        print(param.grad)
+
     # out = net(x).sum()
     # out.backward()
     # print(net.weight.grad)
--- src/numpygrad/nn/mlp.py
+++ src/numpygrad/nn/mlp.py
@@ -6,10 +6,7 @@ from numpygrad.core.array import Array
 class MLP(Module):
     def __init__(self, input_dim: int, hidden_sizes: list[int], output_dim: int):
         dims = [input_dim] + hidden_sizes + [output_dim]
-        self.layers = [
-            Linear(nin, nout, activate=nout != output_dim)
-            for nin, nout in zip(dims[:-1], dims[1:])
-        ]
+        self.layers = [Linear(nin, nout) for nin, nout in zip(dims[:-1], dims[1:])]
 
     def parameters(self) -> list[Array]:
         return [p for layer in self.layers for p in layer.parameters()]
@@ -17,7 +14,7 @@ class MLP(Module):
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({', '.join([repr(layer) for layer in self.layers])})"
 
-    def __call__(self, x: list[Array] | Array) -> Array | list[Array]:
-        for layer in self.layers:
+    def __call__(self, x: Array) -> Array:
+        for layer in self.layers[:-1]:
             x = layer(x)
-        return x
+        return self.layers[-1](x)
