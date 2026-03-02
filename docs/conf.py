3374eaef2475cb037040a0bd4d167162901ac91a
Nick Richardson <njkrichardson@princeton.edu>
2026-03-01 19:11:19 -0500

    [docs]: Add sphinx docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,10 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import importlib.metadata
 
 project = "numpygrad"
 copyright = "2026, Nick Richardson"
 author = "Nick Richardson"
+release = importlib.metadata.version("numpygrad")
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
-templates_path = ["_templates"]
+extensions: list[str] = []
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
 html_theme = "alabaster"
-html_static_path = ["_static"]
